e2752bc771af5d39060035539636895317b11e91
f3azy <francescocarvelli25@gmail.com>
2024-07-01 22:01:44 +0200

    made responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import { HemeraLogo } from "../assets";
+import FooterSocialIconList from "./FooterSocialIconList";
 
 const Footer = () => {
   return (
-    <div className="px-40 z-20 flex justify-between text-white-base py-4 tracking-widest">
-      <p>© COPYRIGHT 2024 HEMERA STUDIOS</p>
-      <p>
-        WEBSITE BY{" "}
-        <a className="underline" href="https://www.fcarvelli.com/" target="_blank">
-          FRANCESCO CARVELLI
-        </a>
-      </p>
+    <div
+      className="px-5 xl:px-40 py-6 xl:py-4 z-20 
+    flex flex-col gap-y-8 
+    text-white-base tracking-widest"
+    >
+      <div className="flex flex-col xl:hidden items-center gap-y-8 font-semibold">
+        <img className="w-[100px]" src={HemeraLogo} alt="logo" />
+        <FooterSocialIconList />
+        <p>ul. Przykladowa 12, 70-000 Szczecin</p>
+        <div>
+          <p>+48 501 463 348</p>
+          <p>+48 505 147 470</p>
+        </div>
+        <p>email@przyklad.com</p>
+      </div>
+      <div className="w-full flex flex-col xl:flex-row justify-between items-center gap-y-2">
+        <p>© COPYRIGHT 2024 HEMERA STUDIOS</p>
+        <p>
+          WEBSITE BY{" "}
+          <a
+            className="underline"
+            href="https://www.fcarvelli.com/"
+            target="_blank"
+          >
+            FRANCESCO CARVELLI
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/components/FooterSocialIconList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SocialIcon from "./SocialIcon";
+import { socialIcons } from "../const/socialIcons";
+
+const FooterSocialIconList = () => {
+  return (
+    <div className="flex gap-x-12">
+      {socialIcons.map((icon) => (
+        <SocialIcon icon={icon} />
+      ))}
+    </div>
+  );
+};
+
+export default FooterSocialIconList;
