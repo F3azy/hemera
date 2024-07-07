1d33cd313bb87fa5a583765c4185969014011498
f3azy <francescocarvelli25@gmail.com>
2024-07-07 23:21:02 +0200

    preparing other pages layoouts

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PageTitle from "../components/PageTitle";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full flex flex-col justify-center items-center gap-y-10 xl:gap-y-12 relative">
+      <PageTitle>NASI KLIENCI</PageTitle>
+    </div>
+  );
 };
 
 export default Testimonials;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PageTitle from "../components/PageTitle";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full flex flex-col justify-center items-center gap-y-10 xl:gap-y-12 relative">
+      <PageTitle>PORTFOLIO</PageTitle>
+    </div>
+  );
 };
 
 export default Work;
