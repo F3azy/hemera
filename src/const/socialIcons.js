e51a940c4ee3ecf8828638ca3b0f0a51657f094f
f3azy <francescocarvelli25@gmail.com>
2024-08-01 19:29:11 +0200

    changes ig url

--- src/const/socialIcons.js
+++ src/const/socialIcons.js
@@ -1,7 +1,11 @@
 import { FaInstagram, FaFacebook, FaYoutube, FaTiktok } from "react-icons/fa";
 
 export const socialIcons = [
-  { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com" },
+  {
+    name: "Instagram",
+    icon: FaInstagram,
+    link: "https://www.instagram.com/hemerastudios_/",
+  },
   ,
   { name: "Facebook", icon: FaFacebook, link: "https://www.facebook.com" },
   ,
