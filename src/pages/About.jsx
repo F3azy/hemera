3f3978ce851487efdda286a948bd5a9091fd80f9
f3azy <francescocarvelli25@gmail.com>
2024-08-01 21:17:31 +0200

    moved highlite

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import ServiceCardList from "../components/ServiceCardList";
 const About = () => {
   return (
     <div className="h-full flex flex-col justify-center items-center gap-y-10 xl:gap-y-12 relative">
-      <div className="bg-brand-primary w-1/2 h-3/4 rounded-full opacity-10 blur-[190px] absolute -right-1/2 -bottom-1/4 xl:-right-72 xl:-bottom-52 z-0" />
+      <div className="bg-brand-primary w-1/2 h-[200px] rounded-full opacity-10 blur-[190px] absolute -right-1/2 bottom-[-50px] xl:-right-72 xl:-bottom-52 z-0" />
       <div className="bg-brand-primary w-1/4 h-1/4 rounded-full opacity-15 blur-[150px] absolute left-0 xl:left-22 top-52 z-0" />
       <PageTitle>KIM JESTEŚMY</PageTitle>
 
